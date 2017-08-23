56ed14bc344b97c33825f1437f568e4e2204f039
Schlueter <bs@schlueter.blue>
2017-08-22 22:20:25 -0400

    Simplify record template with full featured dns record filter

--- filter_plugins/dns_filters.py
+++ filter_plugins/dns_filters.py
@@ -6,42 +6,80 @@ class InvalidRdata(FilterArgumentError):
     def __init__(self, message):
         super().__init__(u'InvalidRdata: {}'.format(message))
 
-def rdata_constructor(constructor):
-    def wrapper(resource):
-        try:
-            return ' '.join([str(field) for field in [resource['name'],
-                                                      resource['ttl'],
-                                                      resource['class'],
-                                                      resource['type'],
-                                                      constructor(resource)]])
-        except KeyError as error:
-            raise InvalidRdata('Missing required field {error}\n{resource}'.format(error=error, resource=resource))
-    return wrapper
+def resource_record_constructor(rdata_constructor):
+    """Returns a resource record constructor which uses the rdata_constructor
+to build the rdata which is joined to the common resource record fields"""
+    def join_rdata_with_common_fields(resource):
+        """Join common resource record fields with return value of the provided
+rdata constructor.
 
-def simple_rdata_constructor_constructor(field):
-    @rdata_constructor
-    def simple_rdata_constructor(resource):
-        rdata = resource.get('value') or resource.get(field)
+If a KeyError is caught during execution, an InvalidRdata exception is raised,
+indictaing that the field for which the KeyError was raised is required."""
+        try:
+            common_fields = [str(field) for field in [
+                resource['name'],
+                resource['ttl'],
+                resource['class'],
+                resource['type']
+            ]]
+            return ' '.join( common_fields + [rdata_constructor(resource)])
+        except KeyError as error:
+            raise InvalidRdata('Missing required field {error}\n{resource}'.format(
+                error=error,
+                resource=resource
+            ))
+    return join_rdata_with_common_fields
+
+def simple_resource_record_constructor_constructor(field):
+    """Returns a simple rdata constructor which uses the provided field or
+the `value` field as the value of the returned rdata."""
+
+    @resource_record_constructor
+    def simple_resource_record_constructor(resource):
+        """Build a simple rdata string from a dictionary containing its specification.
+
+The expected field may be provided to this method's constructor, or its value may be
+set in a field name `value`.
+"""
+        rdata = resource.get(field) or resource.get('value')
         if not rdata:
             raise KeyError(field)
         return rdata
-    return simple_rdata_constructor
+    return simple_resource_record_constructor
 
-@rdata_constructor
+@resource_record_constructor
 def construct_NS_rdata(resource):
-    return resource.get('nsdname',
-                        '{nshost}.ns.{name}'.format( nshost=resource['nshost'],
-                                                         name=resource['name'] ))
+    """Build an NS rdata string from a dictionary containing its specification.
 
-@rdata_constructor
+nsdname is the only value considered. If it is omitted, an appropritae value
+will be created using the name field an additional nshost field. The computed
+value will be `{nshost}.ns.{name}`.
+"""
+    return resource.get('nsdname',
+                        '{nshost}.ns.{name}'.format(
+                            nshost=resource['nshost'],
+                            name=resource['name']
+                        ))
+
+@resource_record_constructor
 def construct_SOA_rdata(resource):
+    """Build an SOA rdata string from a dictionary containing its specification.
+
+Fields are described in section 3.3.13 of https://www.ietf.org/rfc/rfc1035.txt
+
+If mname or rname are omitted, appropriate values for them will be created
+using the name field and an additional nshost field for mname. Those computed
+values will be `{nshost}.ns.{name}` and `hostmaster.{name}` for mname and
+rname respectfully.
+"""
     return '{mname} {rname} {serial} {refresh} {retry} {expire} {minimum}'.format(
          mname=resource.get('mname',
-                            '{nshost}.ns.{name}'.format( nshost=resource['nshost'],
-                                                         name=resource['name'] )),
+                            '{nshost}.ns.{name}'.format(
+                                 nshost=resource['nshost'],
+                                 name=resource['name']
+                             )),
          rname=resource.get('rname',
-                            'hostmaster.{name}'.format( nshost=resource['nshost'],
-                                                         name=resource['name'] )),
+                            'hostmaster.{name}'.format(name=resource['name'])),
          serial=resource['serial'],
          refresh=resource['refresh'],
          retry=resource['retry'],
@@ -49,16 +87,17 @@ def construct_SOA_rdata(resource):
          minimum=resource['minimum']
     )
 
-
-
-resource_record_constructors = dict( A=simple_rdata_constructor_constructor('address'),
-                                     CNAME=simple_rdata_constructor_constructor('cname'),
-                                     TXT=simple_rdata_constructor_constructor('txt-data'),
-                                     PTR=simple_rdata_constructor_constructor('ptrdname'),
-                                     SOA=construct_SOA_rdata,
-                                     NS=construct_NS_rdata )
+resource_record_constructors = dict(
+    A=simple_resource_record_constructor_constructor('address'),
+    CNAME=simple_resource_record_constructor_constructor('cname'),
+    TXT=simple_resource_record_constructor_constructor('txt-data'),
+    PTR=simple_resource_record_constructor_constructor('ptrdname'),
+    SOA=construct_SOA_rdata,
+    NS=construct_NS_rdata
+)
 
 def dns_resource_record(resource):
+     """Build A resource record string from a dictionary containing its specification"""
      constructor = resource_record_constructors[resource['type']]
      return constructor(resource)
 
